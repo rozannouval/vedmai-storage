0540cc00738fd7b0488784f05b4deed65e402e80
rozannouval <rozannouval@gmail.com>
2025-09-07 07:35:09 +0700

    feat: page(sign-in, sign-up), OTP via email and UI design

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">Vedmai Storage - The only storage solution you need.</h1>
+    </div>
+  );
+}
